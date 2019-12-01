4b104246af7e50a4df0e6d291981bb04feef723f
刘小粟 <liuxiaosu@liuxiaosudeMacBook-Pro.local>
2019-12-01 22:33:09 +0800

    12

---
+++ src/cpp/Q12.cpp
@@ -0,0 +1,14 @@
+//
+// Created by 刘小粟 on 2019/12/1.
+//
+
+#include "Q12.h"
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    string s = intToRoman(3891);
+    cout<<s<<endl;
+    return 0;
+}
