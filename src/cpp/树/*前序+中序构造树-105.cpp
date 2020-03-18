4efcb79ff41a4cedf075989c635029278c20101e
刘小粟 <lxs>
2020-03-18 09:15:40 +0800

    3/18

--- src/cpp/树/*前序+中序构造树-105.cpp
+++ src/cpp/树/*前序+中序构造树-105.cpp
@@ -1,51 +1,23 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <unordered_map>
-#include <queue>
-#include <stack>
-#include <unordered_set>
-#include <cmath>
-#include <algorithm>
-using namespace std;
-#define max(a,b) (a)>(b)?(a):(b)
-#define min(a,b) (a)<(b)?(a):(b)
-
-int main() {
-
-    struct ListNode {
-        int val;
-        ListNode *next;
-        ListNode(int x) : val(x), next(NULL) {}
-    };
-    struct TreeNode {
-        int val;
-        TreeNode *left;
-        TreeNode *right;
-        TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-    };
-
-    class Solution {
-    public:
-        TreeNode* buildTree2(vector<int>& pre, int pos, vector<int>& ino, int start, int end) {
-            if (pos >= pre.size())
-                return NULL;
-            int i = start;
-            for (; i <= end; ++i) {
-                if (ino[i] == pre[pos])
-                    break;
-            }
-            TreeNode* root = new TreeNode(pre[pos]);
-            if (i-1 >= start)
-                root->left = buildTree2(pre, pos+1, ino, start, i-1);
-            if (i+1 <= end)
-                root->right = buildTree2(pre, pos+2 , ino , i+1, end);
-            return root;
+class Solution {
+public:
+    TreeNode* buildTree2(vector<int>& pre, int& pos, vector<int>& ino, int start, int end) {
+        if (pos >= pre.size())
+            return NULL;
+        int i = start;
+        for (; i <= end; ++i) {
+            if (ino[i] == pre[pos])
+                break;
         }
-        TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-            int pos = 0;
-            return buildTree2(preorder, pos, inorder, 0, inorder.size()-1);
-        }
-    };
-    return 0;
-}
+        TreeNode* root = new TreeNode(pre[pos]);
+        // 此处pos的值是全局变量，关键在于preorder数组是递归的，所以pos要累加
+        if (i-1 >= start)
+            root->left = buildTree2(pre, ++pos, ino, start, i-1);
+        if (i+1 <= end)
+            root->right = buildTree2(pre, ++pos , ino , i+1, end);
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int pos = 0;
+        return buildTree2(preorder, pos, inorder, 0, inorder.size()-1);
+    }
+};
